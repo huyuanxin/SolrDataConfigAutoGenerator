f0f560cfdf7845f67ab2408f4ebcdb3e51bb53c8
huyuanxin <huyuanxin1999@outlook.com>
2020-12-17 14:32:49 +0800

    GeneratorService的接口

---
+++ src/main/java/yuanxin/solr/generator/api/GeneratorService.java
@@ -0,0 +1,32 @@
+package yuanxin.solr.generator.api;
+
+import org.springframework.stereotype.Service;
+import yuanxin.solr.generator.dto.InputDTO;
+import yuanxin.solr.generator.entity.DataSource;
+import yuanxin.solr.generator.entity.Entity;
+
+import java.util.List;
+
+/**
+ * GeneratorService的接口
+ *
+ * @author huyuanxin
+ */
+@Service("GeneratorService")
+public interface GeneratorService {
+    /**
+     * 生成 {@link DataSource}
+     *
+     * @param inputDTO 输入的需要生成的信息 {@link InputDTO}
+     * @return 生成的 {@link DataSource}
+     */
+    List<DataSource> generatorDataSource(InputDTO inputDTO);
+
+    /**
+     * 生成 {@link Entity}
+     *
+     * @param inputDTO 输入的需要生成的信息 {@link InputDTO}
+     * @return 生成的 {@link Entity}
+     */
+    List<Entity> generatorEntity(InputDTO inputDTO);
+}
