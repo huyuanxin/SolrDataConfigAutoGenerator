acec761d852ccc3a797a30d8c845996947722f5d
huyuanxin <huyuanxin1999@outlook.com>
2020-12-17 14:32:19 +0800

    对应data-config内的<field></field>

---
+++ src/main/java/yuanxin/solr/generator/entity/Field.java
@@ -0,0 +1,27 @@
+package yuanxin.solr.generator.entity;
+
+
+import lombok.Data;
+
+/**
+ * 对应data-config内的<field></field>
+ *
+ * @author huyuanxin
+ */
+@Data
+public class Field {
+    /**
+     * field的columnName属性 {@link String}
+     */
+    public String columnName;
+
+    /**
+     * field的fieldName属性 {@link String}
+     */
+    public String fieldName;
+
+    public Field(String columnName, String fieldName) {
+        this.columnName = columnName;
+        this.fieldName = fieldName;
+    }
+}
